6ddf7cc287a70a712f6749a309bf9f918ba4071d
Valentin Brandl <vbrandl@riseup.net>
2017-05-10 14:09:00 +0200

    Update release script

--- release.sh
+++ release.sh
@@ -6,4 +6,10 @@ echo "Generated manpage"
 cargo pkgbuild
 echo "Generated PKGBUILD"
 
+echo "Checking for outdated crates"
+cargo outdated
+
+echo "Checking for vulnerable crates"
+cargo audit
+
 echo "Next steps: push release, publish crate and update AUR package"
