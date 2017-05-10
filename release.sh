255d2ece266015dc1f1f54273531bad7659f2538
Valentin Brandl <vbrandl@riseup.net>
2017-05-10 11:39:31 +0200

    Add script to automate release process

---
+++ release.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "Have you bumped versions in README.md and Cargo.toml?"
+help2man "cargo run --" > man/man1/skeleton.1
+echo "Generated manpage"
+cargo pkgbuild
+echo "Generated PKGBUILD"
+
+echo "Next steps: push release, publish crate and update AUR package"
