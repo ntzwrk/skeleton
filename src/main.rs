11804fb58abbaecbf5f175a81cd571c7f76e8346
Valentin Brandl <vbrandl@riseup.net>
2017-05-10 10:03:07 +0200

    Removed compiler warnings from clippy
    Used `cargo build --features "clippy"` to detect warnings and fixed them

--- src/main.rs
+++ src/main.rs
@@ -36,7 +36,7 @@ fn main() {
         println!("doing new {}", matches.value_of("NAME").unwrap());
     }
 
-    let config_path = match get_config_path(&lang.to_string()) {
+    let config_path = match get_config_path(lang) {
         Some(p) => p,
         None => {
             println!("Could not find home directory");
@@ -58,14 +58,14 @@ fn main() {
             println!("A file or directory with that name already exists");
             exit(1);
         } else {
-            match create_dir(name.clone()) {
+            match create_dir(name) {
                 Ok(_) => println!("Created directory '{}'", name),
                 Err(e) => {
                     println!("Could not create directory '{}'. {:?}", name, e);
                     exit(1);
                 }
             }
-            if let Err(_) = env::set_current_dir(Path::new(&*name)) {
+            if env::set_current_dir(Path::new(&*name)).is_err() {
                 println!("Could not change directory!");
                 exit(1);
             }
@@ -79,7 +79,7 @@ fn main() {
 
     if let Some(ref includes) = config.include {
         for incl in includes {
-            let path = match get_config_path(&incl) {
+            let path = match get_config_path(incl) {
                 Some(p) => p,
                 None => {
                     println!("Could not resolve home directory");
@@ -105,7 +105,7 @@ fn main() {
 fn mkdir(dirs: &Option<Vec<String>>) {
     if let Some(ref dirs) = *dirs {
         for dir in dirs {
-            if !file_exists(&dir) {
+            if !file_exists(dir) {
                 match create_dir(dir.clone()) {
                     Ok(_) => println!("Created directory '{}'", dir),
                     Err(e) => println!("Could not create directory '{}'. {:?}", dir, e),
@@ -119,7 +119,7 @@ fn mkdir(dirs: &Option<Vec<String>>) {
 
 fn gitignore(gi: &Option<Vec<String>>) {
     if let Some(ref gi) = *gi {
-        let ign = gitignore::get_gitignore(&gi).unwrap();
+        let ign = gitignore::get_gitignore(gi).unwrap();
         let file = OpenOptions::new()
             .create_new(true)
             .append(true)
@@ -138,7 +138,7 @@ fn gitignore(gi: &Option<Vec<String>>) {
 fn touch(files: &Option<Vec<String>>) {
     if let Some(ref fnames) = *files {
         for file in fnames {
-            if !create_file(&file) {
+            if !create_file(file) {
                 println!("Could not create file '{}'", file);
             }
         }
@@ -149,7 +149,7 @@ fn exec_list(cmd_list: &Option<Vec<String>>) {
     if let Some(ref cmds) = *cmd_list {
         for cmd in cmds {
             println!("Executing '{}'", cmd);
-            match exec(&cmd) {
+            match exec(cmd) {
                 Ok((out, err)) => {
                     println!("stdout: {}", out);
                     println!("stderr: {}", err);
@@ -189,9 +189,9 @@ fn process_config(config: Config) {
 /// # Examples
 ///
 /// ```
-/// let path = get_config_path(&"rust".to_string())
+/// let path = get_config_path(&"rust")
 /// ```
-fn get_config_path(lang: &String) -> Option<String> {
+fn get_config_path(lang: &str) -> Option<String> {
     let home = match env::home_dir() {
         Some(p) => p,
         None => return None,
@@ -211,9 +211,9 @@ fn get_config_path(lang: &String) -> Option<String> {
 /// # Examples
 ///
 /// ```
-/// let dir_is_empty(&".".to_string());
+/// let dir_is_empty(&".");
 /// ```
-fn dir_is_empty(dir: &String) -> bool {
+fn dir_is_empty(dir: &str) -> bool {
     let paths = match read_dir(dir) {
         Ok(paths) => paths,
         Err(_) => return false,
@@ -226,14 +226,14 @@ fn dir_is_empty(dir: &String) -> bool {
 }
 
 /// Checks if a file exists
-fn file_exists(name: &String) -> bool {
+fn file_exists(name: &str) -> bool {
     Path::new(&name).exists()
 }
 
 /// Executes a command using `sh` or `cmd.exe` depending on the used operating system.
 ///
 /// Returns either a `Error` or a tuple `(stdout, stderr)`
-fn exec(cmd: &String) -> Result<(String, String)> {
+fn exec(cmd: &str) -> Result<(String, String)> {
     #[cfg(unix)]
     let shell = "sh";
     #[cfg(unix)]
@@ -245,14 +245,14 @@ fn exec(cmd: &String) -> Result<(String, String)> {
 
     let out = Command::new(shell).arg(arg1).arg(cmd).output()?;
 
-    Ok((String::from_utf8(out.stdout).unwrap_or("Could not parse stdout".to_string()),
-        String::from_utf8(out.stderr).unwrap_or("Could not parse stderr".to_string())))
+    Ok((String::from_utf8(out.stdout).unwrap_or_else(|_| "Could not parse stdout".to_string()),
+        String::from_utf8(out.stderr).unwrap_or_else(|_| "Could not parse stderr".to_string())))
 
 }
 
 /// Creates a file if it doesn't exist.
-fn create_file(file_name: &String) -> bool {
-    !file_exists(&file_name) &&
+fn create_file(file_name: &str) -> bool {
+    !file_exists(file_name) &&
     OpenOptions::new()
         .append(true)
         .create(true)
@@ -273,8 +273,8 @@ fn parse_config(file_name: String) -> Result<Config> {
 
 #[test]
 fn test_file_exists() {
-    assert!(file_exists(&"test/foo.toml".to_string()));
-    assert!(!file_exists(&"test/none".to_string()));
+    assert!(file_exists("test/foo.toml"));
+    assert!(!file_exists("test/none"));
 }
 
 #[test]
